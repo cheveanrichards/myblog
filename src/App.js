1a9d95c6df8957cacb593af4aae2c496ab41a1ba
cheveanrichards <richardsca7@outlook.com>
2022-01-11 22:31:57 -0500

    creating fullstack blog project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/Article";
+import ArticleList from "./pages/ArticlesList";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./NavBar";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/about" component={AboutPage} />
+            <Route exact path="/article" component={ArticlePage} />
+            <Route exact path="/article-list" component={ArticleList} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

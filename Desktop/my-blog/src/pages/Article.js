ef56d931f04b8b50cbeb0ffb5e754ff0a4c69d6a
cheveanrichards <richardsca7@outlook.com>
2022-01-11 22:23:22 -0500

    creating fullstack blog project

---
+++ Desktop/my-blog/src/pages/Article.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ArticlePage = () => {
+  return (
+    <>
+      <h1> Hello This is Article Page</h1>
+    </>
+  );
+};
+
+export default ArticlePage;
---
+++ Desktop/my-blog/src/pages/ArticlesList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ArticleList = () => {
+  return (
+    <>
+      <h1> Hello This is ArticleList</h1>
+    </>
+  );
+};
+
+export default ArticleList;

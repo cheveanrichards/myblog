6aa90237a987cd9a1aff90e224cc8bae4f20ece5
cheveanrichards <richardsca7@outlook.com>
2022-01-13 15:08:54 -0500

    configuring client to make request from express server using useffect hook and fetch api

---
+++ src/components/CommentsList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CommentList = ({ comments }) => {
+    return (
+    <>
+        {
+            comments.map((comment, key) => (
+                <div className='comment' key={key}>
+                    <h4>{comment.username}</h4>
+                    <p>{comment.text}</p>
+                </div>
+            ))
+            }
+            </>
+    )
+}
+
+export default CommentList;
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Article from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
-
+import CommentList from '../components/CommentsList'
 const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = Article.find((somearticle) => somearticle.name === name);
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-  fetch('/api/articles/...')
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`)
+      const body = await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    }
+    fetchData()
+  },[name]);
 
 
-  const name = match.params.name;
-  const article = Article.find((somearticle) => somearticle.name === name);
+
   if (!article) {
-   <NotFoundPage/>
+    <NotFoundPage />;
   }
-  const otherArticles = Article.filter(somearticle => somearticle.name !== name)
+  const otherArticles = Article.filter(
+    (somearticle) => somearticle.name !== name
+  );
   return (
     <>
-      <ArticlesList articles={otherArticles} />
       <h1> {article.title}</h1>
+      <p>This post have been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((somedata, key) => (
         <p key={key}>{somedata}</p>
       ))}
+      < CommentList comments={articleInfo.comments}/>
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
     </>
   );
 };
